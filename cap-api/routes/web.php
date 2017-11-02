9853048acda07e0093d6336b6eff37e1cdbf2775
njpol <mail@nielsjan.nl>
2017-11-02 14:22:58 +0100

    Common tenant

--- cap-api/routes/web.php
+++ cap-api/routes/web.php
@@ -20,7 +20,7 @@ Route::get('/logged', function () {
 });
 
 Route::get('/login', function () {
-    return Redirect::to("https://login.microsoftonline.com/b137c280-a798-4a5e-af84-96b82ff4bad4/oauth2/authorize?client_id=51099241-6929-4552-a40e-93c83359a7f1&scope=api&redirect_uri=http://localhost:8000/logged&response_type=code&prompt=consent");
+    return Redirect::to("https://login.microsoftonline.com/common/oauth2/authorize?client_id=51099241-6929-4552-a40e-93c83359a7f1&scope=api&redirect_uri=http://localhost:8000/logged&response_type=code&prompt=consent");
 });
 
 
