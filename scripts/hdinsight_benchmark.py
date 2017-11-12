91846c81edadd4381ca475b16e65b7eafa837242
Tayfun74 <Tayfun74@live.nl>
2017-11-12 13:08:26 +0100

    add hive queries and check if python script will work

--- scripts/hdinsight_benchmark.py
+++ scripts/hdinsight_benchmark.py
@@ -1,9 +1,12 @@
 import requests
 import sys
 import json
+import os
+import glob
+import time
 
 def add_param(url, param_name, param):
-    return url + '&' + param_name + "=" + param
+	return url + '&' + param_name + "=" + param
 
 
 
@@ -23,99 +26,121 @@ print r.text
 url = 'http://193.70.6.75:7778/api/measurement/new?uuid=' + uuid
 url = add_param(url, 'run', '1')
 url = add_param(url, 'successful', '1')
-url = add_param(url, 'q1', '3232')
-url = add_param(url, 'q2', '3232')
-url = add_param(url, 'q3', '3232')
-url = add_param(url, 'q4', '3232')
-url = add_param(url, 'q5', '3232')
-url = add_param(url, 'q6', '3232')
-url = add_param(url, 'q7', '3232')
-url = add_param(url, 'q8', '3232')
-url = add_param(url, 'q9', '3232')
-url = add_param(url, 'q10', '3232')
-url = add_param(url, 'q11', '3232')
-url = add_param(url, 'q12', '3232')
-url = add_param(url, 'q13', '3232')
-url = add_param(url, 'q14', '3232')
-url = add_param(url, 'q15', '3232')
-url = add_param(url, 'q16', '3232')
-url = add_param(url, 'q17', '3232')
-url = add_param(url, 'q18', '3232')
-url = add_param(url, 'q19', '3232')
-url = add_param(url, 'q20', '3232')
-url = add_param(url, 'q21', '3232')
-url = add_param(url, 'q22', '3232')
-print url
+# Run .hive files and time every bechmark
+print "Starting the benchmark"
+hive_queries = sorted(glob.glob("tpch_hive_queries/*.hive"))
+query_num = 1
+run = 1
+for query in hive_queries:
+	start_time = time.time()
+	os.system('hive -f ' + query)
+	end_time = time.time()
+	url = add_param(url, 'q'+ str(query_num) , str(end_time - start_time))
+	++query_num
+
 r = requests.post(url)
 
-
-# Response, status etc
 print r.status_code
-print r.text
-
-url = 'http://193.70.6.75:7778/api/measurement/new?uuid=' + uuid
-url = add_param(url, 'run', '2')
-url = add_param(url, 'successful', '1')
-url = add_param(url, 'q1', '3232')
-url = add_param(url, 'q2', '3232')
-url = add_param(url, 'q3', '3232')
-url = add_param(url, 'q4', '3232')
-url = add_param(url, 'q5', '3232')
-url = add_param(url, 'q6', '3232')
-url = add_param(url, 'q7', '3232')
-url = add_param(url, 'q8', '3232')
-url = add_param(url, 'q9', '3232')
-url = add_param(url, 'q10', '3232')
-url = add_param(url, 'q11', '3232')
-url = add_param(url, 'q12', '3232')
-url = add_param(url, 'q13', '3232')
-url = add_param(url, 'q14', '3232')
-url = add_param(url, 'q15', '3232')
-url = add_param(url, 'q16', '3232')
-url = add_param(url, 'q17', '3232')
-url = add_param(url, 'q18', '3232')
-url = add_param(url, 'q19', '3232')
-url = add_param(url, 'q20', '3232')
-url = add_param(url, 'q21', '3232')
-url = add_param(url, 'q22', '3232')
-print url
-r = requests.post(url)
+# print r.text
 
 
-# Response, status etc
-print r.status_code
-print r.text
+# url = 'http://193.70.6.75:7778/api/measurement/new?uuid=' + uuid
+# url = add_param(url, 'run', '1')
+# url = add_param(url, 'successful', '1')
+# url = add_param(url, 'q1', '3232')
+# url = add_param(url, 'q2', '3232')
+# url = add_param(url, 'q3', '3232')
+# url = add_param(url, 'q4', '3232')
+# url = add_param(url, 'q5', '3232')
+# url = add_param(url, 'q6', '3232')
+# url = add_param(url, 'q7', '3232')
+# url = add_param(url, 'q8', '3232')
+# url = add_param(url, 'q9', '3232')
+# url = add_param(url, 'q10', '3232')
+# url = add_param(url, 'q11', '3232')
+# url = add_param(url, 'q12', '3232')
+# url = add_param(url, 'q13', '3232')
+# url = add_param(url, 'q14', '3232')
+# url = add_param(url, 'q15', '3232')
+# url = add_param(url, 'q16', '3232')
+# url = add_param(url, 'q17', '3232')
+# url = add_param(url, 'q18', '3232')
+# url = add_param(url, 'q19', '3232')
+# url = add_param(url, 'q20', '3232')
+# url = add_param(url, 'q21', '3232')
+# url = add_param(url, 'q22', '3232')
+# print url
+# r = requests.post(url)
 
 
-url = 'http://193.70.6.75:7778/api/measurement/new?uuid=' + uuid
-url = add_param(url, 'run', '3')
-url = add_param(url, 'successful', '1')
-url = add_param(url, 'q1', '3232')
-url = add_param(url, 'q2', '3232')
-url = add_param(url, 'q3', '3232')
-url = add_param(url, 'q4', '3232')
-url = add_param(url, 'q5', '3232')
-url = add_param(url, 'q6', '3232')
-url = add_param(url, 'q7', '3232')
-url = add_param(url, 'q8', '3232')
-url = add_param(url, 'q9', '3232')
-url = add_param(url, 'q10', '3232')
-url = add_param(url, 'q11', '3232')
-url = add_param(url, 'q12', '3232')
-url = add_param(url, 'q13', '3232')
-url = add_param(url, 'q14', '3232')
-url = add_param(url, 'q15', '3232')
-url = add_param(url, 'q16', '3232')
-url = add_param(url, 'q17', '3232')
-url = add_param(url, 'q18', '3232')
-url = add_param(url, 'q19', '3232')
-url = add_param(url, 'q20', '3232')
-url = add_param(url, 'q21', '3232')
-url = add_param(url, 'q22', '3232')
-print url
-r = requests.post(url)
+# # Response, status etc
+# print r.status_code
+# print r.text
+
+# url = 'http://193.70.6.75:7778/api/measurement/new?uuid=' + uuid
+# url = add_param(url, 'run', '2')
+# url = add_param(url, 'successful', '1')
+# url = add_param(url, 'q1', '3232')
+# url = add_param(url, 'q2', '3232')
+# url = add_param(url, 'q3', '3232')
+# url = add_param(url, 'q4', '3232')
+# url = add_param(url, 'q5', '3232')
+# url = add_param(url, 'q6', '3232')
+# url = add_param(url, 'q7', '3232')
+# url = add_param(url, 'q8', '3232')
+# url = add_param(url, 'q9', '3232')
+# url = add_param(url, 'q10', '3232')
+# url = add_param(url, 'q11', '3232')
+# url = add_param(url, 'q12', '3232')
+# url = add_param(url, 'q13', '3232')
+# url = add_param(url, 'q14', '3232')
+# url = add_param(url, 'q15', '3232')
+# url = add_param(url, 'q16', '3232')
+# url = add_param(url, 'q17', '3232')
+# url = add_param(url, 'q18', '3232')
+# url = add_param(url, 'q19', '3232')
+# url = add_param(url, 'q20', '3232')
+# url = add_param(url, 'q21', '3232')
+# url = add_param(url, 'q22', '3232')
+# print url
+# r = requests.post(url)
 
 
-# Response, status etc
-print r.status_code
-print r.text
+# # Response, status etc
+# print r.status_code
+# print r.text
+
+
+# url = 'http://193.70.6.75:7778/api/measurement/new?uuid=' + uuid
+# url = add_param(url, 'run', '3')
+# url = add_param(url, 'successful', '1')
+# url = add_param(url, 'q1', '3232')
+# url = add_param(url, 'q2', '3232')
+# url = add_param(url, 'q3', '3232')
+# url = add_param(url, 'q4', '3232')
+# url = add_param(url, 'q5', '3232')
+# url = add_param(url, 'q6', '3232')
+# url = add_param(url, 'q7', '3232')
+# url = add_param(url, 'q8', '3232')
+# url = add_param(url, 'q9', '3232')
+# url = add_param(url, 'q10', '3232')
+# url = add_param(url, 'q11', '3232')
+# url = add_param(url, 'q12', '3232')
+# url = add_param(url, 'q13', '3232')
+# url = add_param(url, 'q14', '3232')
+# url = add_param(url, 'q15', '3232')
+# url = add_param(url, 'q16', '3232')
+# url = add_param(url, 'q17', '3232')
+# url = add_param(url, 'q18', '3232')
+# url = add_param(url, 'q19', '3232')
+# url = add_param(url, 'q20', '3232')
+# url = add_param(url, 'q21', '3232')
+# url = add_param(url, 'q22', '3232')
+# print url
+# r = requests.post(url)
+
+
+# # Response, status etc
+# print r.status_code
+# print r.text
+
