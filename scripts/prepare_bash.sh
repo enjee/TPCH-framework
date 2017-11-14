f41f73347202cf102a32b743992dac5fcdcfce1c
njpol <mail@nielsjan.nl>
2017-11-14 13:49:51 +0100

    Start benchmark updated

---
+++ scripts/prepare_bash.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Pip installs
+pip install requests
+pip install natsorts
+
+# Install .net core and azcopy
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-xenial-prod xenial main" > /etc/apt/sources.list.d/dotnetdev.list'
+apt-get update
+apt-get -y install dotnet-dev-1.1.4
+
+wget -O azcopy.tar.gz https://aka.ms/downloadazcopyprlinux
+tar -xf azcopy.tar.gz
+./install.sh
+
+git clone https://github.com/enjee/TPCH-framework
