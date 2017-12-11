6f1aab363ba1dfaf81700de1b12d89a0d6660725
Tayfun74 <Tayfun74@live.nl>
2017-12-11 15:06:03 +0100

    Creating keyPairs and using it in SSH requests, still work in progress, so don't run it

--- start_benchmark_amazon.ps1
+++ start_benchmark_amazon.ps1
@@ -45,6 +45,14 @@ if (!(Get-Module -ListAvailable -Name AWSPowerShell)) {
     Write-Output "AWS PowerShell already installed"
 }
 
+# Install ssh for powershell if not exists
+Write-Output "Checking for Posh-SSH"
+if (!(Get-Module -ListAvailable -Name Posh-SSH)) {
+    Install-Module Posh-SSH -Force
+    Import-Module Posh-SSH
+} else {
+    Write-Output "Posh-SSH is already present"
+}
 
 $access_key = "XXXX"
 $secret_key = "XXXX"
@@ -52,14 +60,65 @@ Set-AWSCredential -AccessKey $access_key -SecretKey $secret_key -StoreAs AwsProf
 Initialize-AWSDefaults -ProfileName AwsProfile -Region eu-central-1
 
 
+# Create random cluster name
+$random = -join ((48..57) + (97..122) | Get-Random -Count 16 | % {[char]$_})
+$random = "aws" + $random
+Write-Output ("Random cluster name is: " + $random)
+
+$myPSKeyPair = New-EC2KeyPair -KeyName $random
+$myPSKeyPair.KeyMaterial | Out-File -Encoding ascii myPSKeyPair.pem
+
 Write-Output ("Creating the EMR cluster on your account")
-Start-EMRJobFlow -Name "new_cluster" `
+$job_id = Start-EMRJobFlow -Name $random `
                   -Instances_MasterInstanceType "m4.large" `
                   -Instances_SlaveInstanceType "m4.large" `
                   -Instances_KeepJobFlowAliveWhenNoSteps $true `
                   -Instances_InstanceCount 2 `
                   -Instances_Ec2SubnetId "subnet-af4fbcd2" `
+                  -Instances_Ec2KeyName $myPSKeyPair `
                   -ReleaseLabel "emr-5.10.0" `
                   -JobFlowRole "EMR_EC2_DefaultRole" `
                   -ServiceRole "EMR_DefaultRole" `
-                  -VisibleToAllUsers $true                 
+                  -VisibleToAllUsers $true
+
+Write-Output ("Cluster with id " + $job_id + "is being created")
+
+# Wait until cluster is created
+do {
+    Start-Sleep 10
+    $waitingiswaiting = Get-EMRClusterList -ClusterState "STARTING"
+    $waitcnt = $waitcnt + 10
+    #Write-Host "Starting..." $waitcnt
+}while($waitingiswaiting.Count -eq 1)
+
+# Get cluster information
+$cluster = Get-EMRCluster -ClusterId $job_id
+
+##############################
+# SSH INTO SERVER            #
+##############################
+
+$client = ("hadoop@" + $cluster.MasterPublicDnsName)
+
+$ssh = New-SSHSession $client -KeyFile ".\myPSKeyPair.pem"
+
+Write-Output ("Invoking scripts")
+Invoke-SSHCommand -SSHSession $ssh -Command 'export DEBIAN_FRONTEND=noninteractive'
+Write-Output ("Installing Python modules")
+Invoke-SSHCommand -SSHSession $ssh -Command 'pip install requests'
+Invoke-SSHCommand -SSHSession $ssh -Command 'pip install natsort'
+
+Write-Output ("Cloning GIT repo")
+Invoke-SSHCommand -SSHSession $ssh -Command 'git clone -b development https://github.com/enjee/TPCH-framework'
+
+Write-Output ("Running the Python benchmark")
+$PythonCommand = ($PythonCommand + ' ' + $Size + ' ' + $Repeat + ' ' + $WorkerCount + ' ' + $WorkerNodeType + ' ' + $HeadNodeType + ' ' + $Tag)
+Invoke-SSHCommand -SSHSession $ssh -Command $PythonCHmodCommand -timeout 999999
+Invoke-SSHCommand -SSHSession $ssh -Command $PythonCommand -timeout 999999
+
+Remove-SSHSession -SessionId 0
+
+
+#Remove-EC2KeyPair -KeyName $random
+#Stop-EMRJobFlow-JobFlowId $job_id
+
