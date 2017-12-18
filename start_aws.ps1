dbdd26d42bf88d8922527cf67a9cdec718a57b49
Yke ten Donkelaar <yketd@hotmail.com>
2017-12-18 10:48:32 +0100

    ssh permission updated

--- start_aws.ps1
+++ start_aws.ps1
@@ -84,13 +84,13 @@ $ip1 = new-object Amazon.EC2.Model.IpPermission
 $ip1.IpProtocol = "tcp" 
 $ip1.FromPort = 22 
 $ip1.ToPort = 22 
-$ip1.IpRanges.Add("203.0.113.25/32") 
+$ip1.IpRanges.Add("0.0.0.0/0, ::/0") 
 
 $ip2 = new-object Amazon.EC2.Model.IpPermission 
 $ip2.IpProtocol = "tcp" 
 $ip2.FromPort = 3389 
 $ip2.ToPort = 3389 
-$ip2.IpRanges.Add("203.0.113.25/32") 
+$ip2.IpRanges.Add("0.0.0.0/0, ::/0") 
 
 Grant-EC2SecurityGroupIngress -GroupId $groupid -IpPermissions @( $ip1, $ip2 )	
 
