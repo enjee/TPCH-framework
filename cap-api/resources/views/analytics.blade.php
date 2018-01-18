30771e76387733080f8f647fd3cb65db19e09558
Niels Jan van de Pol <njpol@Nielss-MacBook-Pro.local>
2018-01-18 14:03:51 +0100

    Add dropdown to analytics

--- cap-api/resources/views/analytics.blade.php
+++ cap-api/resources/views/analytics.blade.php
@@ -4,6 +4,20 @@
 
 </div><div class="content-wrapper">
     <div class="container-fluid">
+
+
+    <div class="dropdown">
+      <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Select testset size
+      </button>
+      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+        <a class="dropdown-item" href="analytics/1">1 GB</a>
+        <a class="dropdown-item" href="analytics/10">10 GB</a>
+        <a class="dropdown-item" href="analytics/100">100 GB</a>
+        <a class="dropdown-item" href="analytics/1000">1000 GB</a>
+      </div>
+
+
         <div id="main">
             <svg width="960" height="500"></svg>
         </div>
