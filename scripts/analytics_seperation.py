2199a73038a6c1b0f674d0e294e392794211bdd1
enjee <ahp_vdpol@hotmail.com>
2018-01-22 15:57:36 +0100

    Move back jquery

--- scripts/analytics_seperation.py
+++ scripts/analytics_seperation.py
@@ -56,7 +56,7 @@ for line in s.split("\n"):
         continue
     if "@" in line:
         continue
-    base += line.replace("') }}", "").replace("{{ asset('", "").replace("href=\"/timeline\"", "href=\"/index.html\"").replace("href=\"/analytics\"", "href=\"/index.html\"") + "\n"
+    base += line.replace("') }}", "").replace("{{ asset('", "").replace("href=\"/timeline\"", "href=\"/index.html\"").replace("href=\"/analytics\"", "href=\"/index\"") + "\n"
 f = open(target_dir + "index.html", 'w')
 f.write(base)
 f.close()
